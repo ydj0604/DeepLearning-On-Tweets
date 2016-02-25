7cf6a0af3008236d107b00d4b1fa09b6cfac63e0
Dong Jin Yu <Jin@vpn-128-97-245-74.host.ucla.edu>
2016-02-24 21:27:58 -0800

    Separate sentence-modeling

--- sentence-modeling/logger.py
+++
@@ -1,7 +0,0 @@
-class Logger(object):
-    def __init__(self, file_path):
-        self.file_path = file_path
-
-    def write(self, msg):
-        with open(self.file_path, "a") as f:
-            f.write(msg + '\n')
